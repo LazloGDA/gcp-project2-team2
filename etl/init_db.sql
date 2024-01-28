0a67257f803ee8bb5d4c174b3339e3d35378edf5
inuit5 <gall.nikoletta5@gmail.com>
2024-01-28 19:42:43 +0000

    correct punctuation

--- etl/init_db.sql
+++ etl/init_db.sql
@@ -1,6 +1,6 @@
 CREATE TABLE weather (
     city_number INT NOT NULL AUTO_INCREMENT, 
-    PRIMARY KEY(city_number)
+    PRIMARY KEY(city_number),
     city_name VARCHAR(255), 
     country VARCHAR(255), 
     latitude DECIMAL(10, 8) NOT NULL, 

c8a88fe00a44da71dcdf5f3084e592271ee2a26c
inuit5 <gall.nikoletta5@gmail.com>
2024-01-28 19:30:55 +0000

    Add create table script

---
+++ etl/init_db.sql
@@ -0,0 +1,12 @@
+CREATE TABLE weather (
+    city_number INT NOT NULL AUTO_INCREMENT, 
+    PRIMARY KEY(city_number)
+    city_name VARCHAR(255), 
+    country VARCHAR(255), 
+    latitude DECIMAL(10, 8) NOT NULL, 
+    longitude DECIMAL(11, 8) NOT NULL, 
+    temperature INT, 
+    weather VARCHAR(255), 
+    weather_desc VARCHAR(255)
+     
+);

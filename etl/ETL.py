8e344a861e2d7ef5d6420e716ebc284be417c9c0
inuit5 <gall.nikoletta5@gmail.com>
2024-01-30 14:30:25 +0000

    modify data transformation

--- etl/ETL.py
+++ etl/ETL.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import mysql.connector
 from pymongo import MongoClient
+import uuid
 
 # MongoDB connection settings
 mongo_uri = "mongodb://mongodb:27017/"
@@ -31,41 +32,41 @@ data = list(mongo_collection.find({}))
 
 # Convert to Pandas DataFrame
 
-#weather_data = []
-#for doc in data:
-    #weather_record = {
-        #'city_number': doc.get('city_number', 'Default Value'),
-        #'city_name': doc.get('name', 'Default Value'),
-        #'country': doc.get('sys', {}).get('country', 'Default Value'),
-        #'latitude': doc.get('coord', {}).get('lat', 'Default Value'),
-        #'longitude': doc.get('coord', {}).get('lon', 'Default Value'),
-        #'temperature': doc.get('main', {}).get('temp', 'Default Value'),
-        #'weather': doc.get('weather', [{'main': 'Default Value'}])[0].get('main', 'Default Value'),
-        #'weather_desc': doc.get('weather', [{'description': 'Default Value'}])[0].get('description', 'Default Value')
-    #}
-    #weather_data.append(weather_record)
-
-
-#df = pd.DataFrame(weather_data)
-
-df = pd.DataFrame()
-
-if len(data) > 0 and isinstance(data[0], dict):
-    doc = data[0]
+weather_data = []
+for doc in data:
     weather_record = {
-        'city_number': doc.get('city_number', 0),
+        'city_number': str(uuid.uuid4()),
         'city_name': doc.get('name', 'Default Value'),
-	'country': doc.get('sys', {}).get('country', 'Default Value'),
-    	'latitude': doc.get('coord', {}).get('lat', 'Default Value'),
-    	'longitude': doc.get('coord', {}).get('lon', 'Default Value'),
-    	'temperature': doc.get('main', {}).get('temp', 'Default Value'),
-    	'weather': doc.get('weather', [{'main': 'Default Value'}])[0].get('main', 'Default Value'),
-    	'weather_desc': doc.get('weather', [{'description': 'Default Value'}])[0].get('description', 'Default Value')
-}
-    df = pd.DataFrame([weather_record])
+        'country': doc.get('sys', {}).get('country', 'Default Value'),
+        'latitude': doc.get('coord', {}).get('lat', 'Default Value'),
+        'longitude': doc.get('coord', {}).get('lon', 'Default Value'),
+        'temperature': doc.get('main', {}).get('temp', 'Default Value'),
+        'weather': doc.get('weather', [{'main': 'Default Value'}])[0].get('main', 'Default Value'),
+        'weather_desc': doc.get('weather', [{'description': 'Default Value'}])[0].get('description', 'Default Value')
+    }
+    weather_data.append(weather_record)
+
+
+df = pd.DataFrame(weather_data)
+
+#df = pd.DataFrame()
+
+#if len(data) > 0 and isinstance(data[0], dict):
+    #doc = data[0]
+    #weather_record = {
+        #'city_number': doc.get('city_number', 0),
+        #'city_name': doc.get('name', 'Default Value'),
+	#'country': doc.get('sys', {}).get('country', 'Default Value'),
+    #	'latitude': doc.get('coord', {}).get('lat', 'Default Value'),
+    #	'longitude': doc.get('coord', {}).get('lon', 'Default Value'),
+    #	'temperature': doc.get('main', {}).get('temp', 'Default Value'),
+    #	'weather': doc.get('weather', [{'main': 'Default Value'}])[0].get('main', 'Default Value'),
+    #	'weather_desc': doc.get('weather', [{'description': 'Default Value'}])[0].get('description', 'Default Value')
+#}
+#    df = pd.DataFrame([weather_record])
 
 if not df.empty:
-    print(df)
+    #print(df)
     # Connect to MySQL
     mysql_conn = mysql.connector.connect(**mysql_config)
     cursor = mysql_conn.cursor()
@@ -87,3 +88,4 @@ if not df.empty:
 
 else:
     print('DF is empty')
+
